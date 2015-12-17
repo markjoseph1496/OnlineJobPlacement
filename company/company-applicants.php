9c1842bfb770ba080baa24f5898d844f135a6e38
Mark Joseph Cinco <markjoseph1496@yahoo.com>
2015-12-18 00:18:49 +0800

    Commit chuchu

--- company/company-applicants.php
+++ company/company-applicants.php
@@ -19,58 +19,58 @@
   <meta name="author" content="iThemesLab">
 
   <!-- Bootstrap CSS  -->
-  <link rel="stylesheet" href="asset/css/bootstrap.min.css" type="text/css" media="screen">
+  <link rel="stylesheet" href="../asset/css/bootstrap.min.css" type="text/css" media="screen">
 
   <!-- Font Awesome CSS -->
-  <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css" media="screen">
+  <link rel="stylesheet" href="../css/font-awesome.min.css" type="text/css" media="screen">
 
   <!-- Slicknav -->
-  <link rel="stylesheet" type="text/css" href="css/slicknav.css" media="screen">
+  <link rel="stylesheet" type="text/css" href="../css/slicknav.css" media="screen">
 
   <!-- Margo CSS Styles  -->
-  <link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
+  <link rel="stylesheet" type="text/css" href="../css/style.css" media="screen">
 
   <!-- Responsive CSS Styles  -->
-  <link rel="stylesheet" type="text/css" href="css/responsive.css" media="screen">
+  <link rel="stylesheet" type="text/css" href="../css/responsive.css" media="screen">
 
   <!-- Css3 Transitions Styles  -->
-  <link rel="stylesheet" type="text/css" href="css/animate.css" media="screen">
+  <link rel="stylesheet" type="text/css" href="../css/animate.css" media="screen">
 
   <!-- OJPMS CSS  -->
-  <link rel="stylesheet" type="text/css" href="css/ojpms-style.css" media="screen">
+  <link rel="stylesheet" type="text/css" href="../css/ojpms-style.css" media="screen">
 
   <!-- Color CSS Styles  -->
-  <link rel="stylesheet" type="text/css" href="css/colors/red.css" title="red" media="screen" />
-  <link rel="stylesheet" type="text/css" href="css/colors/jade.css" title="jade" media="screen" />
-  <link rel="stylesheet" type="text/css" href="css/colors/blue.css" title="blue" media="screen" />
-  <link rel="stylesheet" type="text/css" href="css/colors/beige.css" title="beige" media="screen" />
-  <link rel="stylesheet" type="text/css" href="css/colors/cyan.css" title="cyan" media="screen" />
-  <link rel="stylesheet" type="text/css" href="css/colors/green.css" title="green" media="screen" />
-  <link rel="stylesheet" type="text/css" href="css/colors/orange.css" title="orange" media="screen" />
-  <link rel="stylesheet" type="text/css" href="css/colors/peach.css" title="peach" media="screen" />
-  <link rel="stylesheet" type="text/css" href="css/colors/pink.css" title="pink" media="screen" />
-  <link rel="stylesheet" type="text/css" href="css/colors/purple.css" title="purple" media="screen" />
-  <link rel="stylesheet" type="text/css" href="css/colors/sky-blue.css" title="sky-blue" media="screen" />
-  <link rel="stylesheet" type="text/css" href="css/colors/yellow.css" title="yellow" media="screen" />
+  <link rel="stylesheet" type="text/css" href="../css/colors/red.css" title="red" media="screen" />
+  <link rel="stylesheet" type="text/css" href="../css/colors/jade.css" title="jade" media="screen" />
+  <link rel="stylesheet" type="text/css" href="../css/colors/blue.css" title="blue" media="screen" />
+  <link rel="stylesheet" type="text/css" href="../css/colors/beige.css" title="beige" media="screen" />
+  <link rel="stylesheet" type="text/css" href="../css/colors/cyan.css" title="cyan" media="screen" />
+  <link rel="stylesheet" type="text/css" href="../css/colors/green.css" title="green" media="screen" />
+  <link rel="stylesheet" type="text/css" href="../css/colors/orange.css" title="orange" media="screen" />
+  <link rel="stylesheet" type="text/css" href="../css/colors/peach.css" title="peach" media="screen" />
+  <link rel="stylesheet" type="text/css" href="../css/colors/pink.css" title="pink" media="screen" />
+  <link rel="stylesheet" type="text/css" href="../css/colors/purple.css" title="purple" media="screen" />
+  <link rel="stylesheet" type="text/css" href="../css/colors/sky-blue.css" title="sky-blue" media="screen" />
+  <link rel="stylesheet" type="text/css" href="../css/colors/yellow.css" title="yellow" media="screen" />
 
 
   <!-- Margo JS  -->
-  <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
-  <script type="text/javascript" src="js/jquery.migrate.js"></script>
-  <script type="text/javascript" src="js/modernizrr.js"></script>
-  <script type="text/javascript" src="asset/js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="js/jquery.fitvids.js"></script>
-  <script type="text/javascript" src="js/owl.carousel.min.js"></script>
-  <script type="text/javascript" src="js/nivo-lightbox.min.js"></script>
-  <script type="text/javascript" src="js/jquery.isotope.min.js"></script>
-  <script type="text/javascript" src="js/jquery.appear.js"></script>
-  <script type="text/javascript" src="js/count-to.js"></script>
-  <script type="text/javascript" src="js/jquery.textillate.js"></script>
-  <script type="text/javascript" src="js/jquery.lettering.js"></script>
-  <script type="text/javascript" src="js/jquery.easypiechart.min.js"></script>
-  <script type="text/javascript" src="js/jquery.nicescroll.min.js"></script>
-  <script type="text/javascript" src="js/jquery.parallax.js"></script>
-  <script type="text/javascript" src="js/jquery.slicknav.js"></script>
+  <script type="text/javascript" src="../js/jquery.min.js"></script>
+  <script type="text/javascript" src="../js/jquery.migrate.js"></script>
+  <script type="text/javascript" src="../js/modernizrr.js"></script>
+  <script type="text/javascript" src="../asset/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="../js/jquery.fitvids.js"></script>
+  <script type="text/javascript" src="../js/owl.carousel.min.js"></script>
+  <script type="text/javascript" src="../js/nivo-lightbox.min.js"></script>
+  <script type="text/javascript" src="../js/jquery.isotope.min.js"></script>
+  <script type="text/javascript" src="../js/jquery.appear.js"></script>
+  <script type="text/javascript" src="../js/count-to.js"></script>
+  <script type="text/javascript" src="../js/jquery.textillate.js"></script>
+  <script type="text/javascript" src="../js/jquery.lettering.js"></script>
+  <script type="text/javascript" src="../js/jquery.easypiechart.min.js"></script>
+  <script type="text/javascript" src="../js/jquery.nicescroll.min.js"></script>
+  <script type="text/javascript" src="../js/jquery.parallax.js"></script>
+  <script type="text/javascript" src="../js/jquery.slicknav.js"></script>
 
   <!--[if IE 8]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
   <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
@@ -237,7 +237,7 @@
     <!-- End Header Section -->
 
 <!-- Start Page Banner -->
-    <div class="page-banner" style="padding:40px 0; background: url(images/slide-02-bg.jpg) center #f9f9f9;">
+    <div class="page-banner" style="padding:40px 0; center #f9f9f9;">
       <div class="container">
         <div class="row">
           <div class="col-md-6">
@@ -320,7 +320,7 @@
 
 
 
-  <script type="text/javascript" src="js/script.js"></script>
+  <script type="text/javascript" src="../js/script.js"></script>
 
 </body>
 
