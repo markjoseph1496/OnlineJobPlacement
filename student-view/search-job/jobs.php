d9d9480e08c3e9d37c71e3cfd34c889783c66bed
Aira Jane <aijeyn@gmail.com>
2015-12-19 03:17:36 +0800

    q

--- student-view/search-job/jobs.php
+++ student-view/search-job/jobs.php
@@ -111,14 +111,9 @@ $result = mysql_query($qry);
                 <div class="container">
                     <div class="row">
                         <div class="col-md-7">
-                            <!-- Start Contact Info 
+                            <!-- Start Contact Info -->
                             <ul class="contact-details">
-                                <li><a href="#"><i class="fa fa-map-marker"></i> House-54/A, London, UK</a>
-                                </li>
-                                <li><a href="#"><i class="fa fa-envelope-o"></i> info@yourcompany.com</a>
-                                </li>
-                                <li><a href="#"><i class="fa fa-phone"></i> +12 345 678 000</a>
-                                </li>
+                                <li><a href="#"><i class="fa fa-hashtag"></i> 008-2012-0805</a></li>
                             </ul>
                             <!-- End Contact Info -->
                         </div>
@@ -126,7 +121,13 @@ $result = mysql_query($qry);
                         <div class="col-md-5">
                         <!-- Start Social Links -->
                             <ul class="social-list">
-                               <li class="profile-name">
+                                <li class="profile-name">
+                                    <a class="bell itl-tooltip" data-placement="bottom" title="" href="#" data-original-title="Notification"><i class="fa fa-bell"></i></a>                            
+                                </li>
+                                <li class="profile-name">
+                                    &nbsp;
+                                </li>
+                                <li class="profile-name">
                                     <i class="fa fa-user"></i> Hello, <b>Aira Jane Cruz</b>                               
                                 </li>
                             </ul>
