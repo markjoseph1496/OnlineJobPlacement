c42a712dc8745c6400a2e831ab67f744eabfb897
chairamirez <ramirezchai@gmail.com>
2015-12-18 16:08:13 -0800

    added server config

--- connection.php
+++ connection.php
@@ -1,5 +1,9 @@
 <?php
 
+//$hostname = "mysql.hostinger.ph";
+//$user = "u987316885_e2edb";
+//$password = "o2ehQX3wqO";
+//$database ="u987316885_e2edb";
 
 $hostname = "localhost:3306";
 $user = "root";
