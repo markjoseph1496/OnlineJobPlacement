2fa513699eb995a2bd0c0970ebb29a9578914fcf
chairamirez <ramirezchai@gmail.com>
2015-12-19 03:24:53 -0800

    erh

--- student-view/myinfo/references.php
+++ student-view/myinfo/references.php
@@ -177,16 +177,11 @@ $result = mysql_query($qry);
                         <a class="navbar-brand" href="">Online JPMS</a>
                     </div>
                     <div class="navbar-collapse collapse">
-                    <!-- Stat Search -->
-                        <div class="search-side">
-                            <a class="show-search"><i class="fa fa-search"></i></a>
-                            <div class="search-form">
-                                <form autocomplete="off" role="search" method="get" class="searchform" action="#">
-                                    <input type="text" value="" name="s" id="s" placeholder="Search the site...">
-                                </form>
-                            </div>
+                   <!-- Sign-out -->
+                        <div class="signout-side">
+                               <a class="show-signout"><i class="fa fa-sign-out"></i></a>
                         </div>
-                        <!-- End Search -->
+                     <!-- End Sign-out -->
                         <!-- Start Navigation List -->
                         <ul class="nav navbar-nav navbar-right">
                             <li>
