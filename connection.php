1e2f5c9322b1f9b8df874d83c998b4776949cded
chairamirez <ramirezchai@gmail.com>
2015-12-18 16:04:57 -0800

    connection.php edit by chai (its me ghabx)

--- connection.php
+++ connection.php
@@ -1,9 +1,11 @@
 <?php
 
-$hostname = "mysql.hostinger.ph";
-$user = "u987316885_e2edb";
-$password = "o2ehQX3wqO";
-$database ="u987316885_e2edb";
+
+$hostname = "localhost:3306";
+$user = "root";
+$password = "";
+$database ="e2edb";
+
 
 $bd = mysql_connect($hostname,$user,$password) or die ("Could not connect"); //connection of php to navicat
 		//sensitive yung spacing... dito lang ^
