6e39f56342649e123ca6931413179497fe3bfdfa
Aira Jane <aijeyn@gmail.com>
2015-12-22 11:55:23 +0800

    q

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
     <!-- Page Description and Author -->
-    <meta name="description" content="Margo - Responsive HTML5 Template">
-    <meta name="author" content="iThemesLab">
+    <meta name="description" content="Responsive HTML5 Template">
+    <meta name="author" content="">
 
     <!-- Bootstrap CSS  -->
     <link rel="stylesheet" href="asset/css/bootstrap.min.css" type="text/css" media="screen">
