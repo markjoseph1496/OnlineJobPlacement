6ce334358e949f28f558ef72686c9d7c218c282f
Mark Joseph Cinco <markjoseph1496@yahoo.com>
2015-12-26 19:00:59 +0800

    test

--- connection.php
+++ connection.php
@@ -12,8 +12,8 @@ $database ="u987316885_e2edb";
 
 
 
-$bd = mysql_connect($hostname,$user,$password) or die ("Could not connect"); //connection of php to navicat
+$bd = mysqli_connect($hostname,$user,$password) or die ("Could not connect"); //connection of php to navicat
 		//sensitive yung spacing... dito lang ^
- mysql_select_db($database,$bd) or die("could not select"); //selection ng DATABASE name w/c is mydatabase;
+ mysqli_select_db($database,$bd) or die("could not select"); //selection ng DATABASE name w/c is mydatabase;
 
 ?>
