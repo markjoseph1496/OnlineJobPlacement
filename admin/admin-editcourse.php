a912c30db279e51124f555c8b1ce80f2d0b55994
Mark Joseph Cinco <markjoseph1496@yahoo.com>
2015-12-28 13:12:30 +0800

    QWERTY

---
+++ admin/admin-editcourse.php
@@ -0,0 +1,342 @@
+<?php
+include('../connection.php');
+
+$CourseID = $_GET['id'];
+$query = "SELECT * FROM coursetbl WHERE CourseID = '$CourseID'";
+$result = mysql_query($query);
+while ($query = mysql_fetch_array($result)) {
+    $coursetitle = $query['CourseTitle'];
+    $coursecode = $query['CourseCode'];
+    $coursedescription = $query['CourseDescription'];
+
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <!-- Basic -->
+    <title>OJPMS</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet"/>
+
+    <!-- BootstrapValidator CSS -->
+    <link href="../css/bootstrapValidator.min.css" rel="stylesheet"/>
+    <link href="../css/basic-template.css" rel="stylesheet"/>
+    <!-- jQuery and Bootstrap JS -->
+    <script src="../js/jquery.min.js" type="text/javascript"></script>
+    <script src="../js/bootstrap.min.js" type="text/javascript"></script>
+
+    <!-- BootstrapValidator -->
+    <script src="../js/bootstrapValidator.min.js" type="text/javascript"></script>
+
+
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="../css/font-awesome.min.css" type="text/css" media="screen">
+
+    <!-- Slicknav -->
+    <link rel="stylesheet" type="text/css" href="../css/slicknav.css" media="screen">
+
+    <!-- Margo CSS Styles  -->
+    <link rel="stylesheet" type="text/css" href="../css/style.css" media="screen">
+
+    <!-- Responsive CSS Styles  -->
+    <link rel="stylesheet" type="text/css" href="../css/responsive.css" media="screen">
+
+    <!-- Css3 Transitions Styles  -->
+    <link rel="stylesheet" type="text/css" href="../css/animate.css" media="screen">
+
+    <!-- OJPMS CSS  -->
+    <link rel="stylesheet" type="text/css" href="../css/ojpms-style.css" media="screen">
+
+    <!-- Color CSS Styles  -->
+    <link rel="stylesheet" type="text/css" href="../css/colors/red.css" title="red" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="../css/colors/jade.css" title="jade" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="../css/colors/blue.css" title="blue" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="../css/colors/beige.css" title="beige" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="../css/colors/cyan.css" title="cyan" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="../css/colors/green.css" title="green" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="../css/colors/orange.css" title="orange" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="../css/colors/peach.css" title="peach" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="../css/colors/pink.css" title="pink" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="../css/colors/purple.css" title="purple" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="../css/colors/sky-blue.css" title="sky-blue" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="../css/colors/yellow.css" title="yellow" media="screen"/>
+
+
+    <!-- Margo JS  -->
+    <script type="text/javascript" src="../js/jquery.migrate.js"></script>
+    <script type="text/javascript" src="../js/modernizrr.js"></script>
+    <script type="text/javascript" src="../js/jquery.fitvids.js"></script>
+    <script type="text/javascript" src="../js/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="../js/nivo-lightbox.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.isotope.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.appear.js"></script>
+    <script type="text/javascript" src="../js/count-to.js"></script>
+    <script type="text/javascript" src="../js/jquery.textillate.js"></script>
+    <script type="text/javascript" src="../js/jquery.lettering.js"></script>
+    <script type="text/javascript" src="../js/jquery.easypiechart.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.nicescroll.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.parallax.js"></script>
+    <script type="text/javascript" src="../js/jquery.slicknav.js"></script>
+
+</head>
+
+<body>
+<!-- Full Body Container -->
+<div id="container">
+
+
+    <!-- Start Header Section -->
+    <div class="hidden-header"></div>
+    <header class="clearfix">
+
+        <!-- Start Top Bar -->
+        <div class="top-bar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <!-- Start Contact Info
+                        <ul class="contact-details">
+                          <li><a href="#"><i class="fa fa-map-marker"></i> House-54/A, London, UK</a>
+                          </li>
+                          <li><a href="#"><i class="fa fa-envelope-o"></i> info@yourcompany.com</a>
+                          </li>
+                          <li><a href="#"><i class="fa fa-phone"></i> +12 345 678 000</a>
+                          </li>
+                        </ul>
+                         End Contact Info -->
+                    </div>
+                    <!-- .col-md-6 -->
+                    <div class="col-md-6">
+                        <!-- Start Social Links
+                        <ul class="social-list">
+                          <li>
+                            <a class="facebook itl-tooltip" data-placement="bottom" title="Facebook" href="#"><i class="fa fa-facebook"></i></a>
+                          </li>
+                          <li>
+                            <a class="twitter itl-tooltip" data-placement="bottom" title="Twitter" href="#"><i class="fa fa-twitter"></i></a>
+                          </li>
+                          <li>
+                            <a class="google itl-tooltip" data-placement="bottom" title="Google Plus" href="#"><i class="fa fa-google-plus"></i></a>
+                          </li>
+                          <li>
+                            <a class="dribbble itl-tooltip" data-placement="bottom" title="Dribble" href="#"><i class="fa fa-dribbble"></i></a>
+                          </li>
+                          <li>
+                            <a class="linkdin itl-tooltip" data-placement="bottom" title="Linkedin" href="#"><i class="fa fa-linkedin"></i></a>
+                          </li>
+                          <li>
+                            <a class="flickr itl-tooltip" data-placement="bottom" title="Flickr" href="#"><i class="fa fa-flickr"></i></a>
+                          </li>
+                          <li>
+                            <a class="tumblr itl-tooltip" data-placement="bottom" title="Tumblr" href="#"><i class="fa fa-tumblr"></i></a>
+                          </li>
+                          <li>
+                            <a class="instgram itl-tooltip" data-placement="bottom" title="Instagram" href="#"><i class="fa fa-instagram"></i></a>
+                          </li>
+                          <li>
+                            <a class="vimeo itl-tooltip" data-placement="bottom" title="vimeo" href="#"><i class="fa fa-vimeo-square"></i></a>
+                          </li>
+                          <li>
+                            <a class="skype itl-tooltip" data-placement="bottom" title="Skype" href="#"><i class="fa fa-skype"></i></a>
+                          </li>
+                        </ul>
+                         End Social Links -->
+                    </div>
+                    <!-- .col-md-6 -->
+                </div>
+                <!-- .row -->
+            </div>
+            <!-- .container -->
+        </div>
+        <!-- .top-bar -->
+        <!-- End Top Bar -->
+
+
+        <!-- Start  Logo & Naviagtion  -->
+        <div class="navbar navbar-default navbar-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <!-- Stat Toggle Nav Link For Mobiles -->
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                    <!-- End Toggle Nav Link For Mobiles -->
+                    <a class="navbar-brand" href="index.html">
+                        OJPMS
+                    </a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <!-- Sign-out -->
+                    <div class="signout-side">
+                        <a class="show-signout"><i class="fa fa-sign-out"></i></a>
+                    </div>
+                    <!-- End Sign-out -->
+                    <!-- Start Navigation List -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a href="admin.php">Home</a>
+                        </li>
+                        <li>
+                            <a href="admin-reports.php">Reports</a>
+                        </li>
+                        <li>
+                            <a href="admin-account.php">Account</a>
+                        </li>
+                        <li>
+                            <a href="admin-requested.php">Requested</a>
+                        </li>
+                        <li>
+                            <a href="admin-companylist.php">Company List</a>
+                        </li>
+                        <li><a href="admin-adviser.php">Adviser List</a>
+                        </li>
+                        <li>
+                            <a> Maintenance</a>
+                            <ul class="dropdown">
+                                <li><a href="admin-maintenance.php">Courses</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <!-- End Navigation List -->
+                </div>
+            </div>
+            <!-- Mobile Menu Start -->
+            <ul class="wpb-mobile-menu">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="admin.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="admin-reports.php">Reports</a>
+                    </li>
+                    <li>
+                        <a href="admin-account.php">Account</a>
+                    </li>
+                    <li>
+                        <a href="admin-requested.php">Requested</a>
+                    </li>
+                    <li>
+                        <a href="admin-companylist.php">Company List</a>
+                    </li>
+                    <li><a href="admin-adviser.php">Adviser List</a>
+                    </li>
+                    <li>
+                        <a> Maintenance</a>
+                        <ul class="dropdown">
+                            <li><a href="admin-maintenance.php">Courses</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </ul>
+    </header>
+    <!-- Mobile Menu End -->
+
+</div>
+<!-- End Header Logo & Naviagtion -->
+
+</header>
+<!-- End Header Section -->
+
+<!-- Start Page Banner -->
+<div class="page-banner" style="padding:40px 0; center #f9f9f9;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Maintenance</h2>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Page Banner -->
+
+<!--Content-->
+<br><br><br>
+<form id="EditCourse" name="EditCourse" autocomplete="off" method="POST" action="functions.php">
+    <input type="text" class="form-control" id="CourseID" name="CourseID" style="display: none;"
+           value="<?php echo $CourseID; ?>">
+    <div class="container">
+        <div class="col-md-12">
+            <div class="row field">
+                <div class="col-md-2 fieldcol">
+                    <label = "usr" class = "control-label"> Course Title </label>
+                </div>
+                <div class="col-md-4 fieldcol">
+                    <div class="form-group">
+                        <input type="text" name="coursetitle" class="form-control" id="coursetitle"
+                               value="<?php echo $coursetitle; ?>">
+                    </div>
+                </div>
+            </div>
+            <div class="row field">
+                <div class="col-md-2 fieldcol">
+                    <label = "usr" class = "control-label"> Course Code:</label>
+                </div>
+                <div class="col-md-4 fieldcol">
+                    <div class="form-group">
+                        <input type="text" name="coursecode" class="form-control" id="coursecode"
+                               value="<?php echo $coursecode; ?>">
+                    </div>
+                </div>
+            </div>
+            <div class="row field">
+                <div class="col-md-2 fieldcol">
+                    <label = "usr" class = "control-label"> Course Description: </label>
+                </div>
+                <div class="col-md-4 fieldcol">
+                    <div class="form-group">
+                        <input type="text" name="coursedesc" class="form-control" id="coursedesc"
+                               value="<?php echo $coursedescription; ?>">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <button type="submit" class="btn btn-lg btn-hg btn-primary"><b> Save </b></button>
+        &nbsp;
+        <button type="submit" class="btn btn-lg btn-hg btn-primary"><b> Cancel </b></button>
+
+    </div>
+</form>
+<script type="text/javascript" src="../js/script.js"></script>
+</body>
+</html>
+<script type="text/javascript">
+    $(document).ready(function () {
+        var validator = $("#EditCourse").bootstrapValidator({
+            feedbackIcons: {
+                valid: "glyphicon glyphicon-ok",
+                invalid: "glyphicon glyphicon-remove",
+                validating: "glyphicon glyphicon-refresh"
+            },
+            fields: {
+                coursetitle: {
+                    validators: {
+                        notEmpty: {
+                            message: "Course title is required"
+                        }
+                    }
+                },
+                coursecode: {
+                    validators: {
+                        notEmpty: {
+                            message: "Course code is required"
+                        }
+                    }
+                },
+                coursedesc: {
+                    validators: {
+                        notEmpty: {
+                            message: "Course description is required"
+                        }
+                    }
+                }
+            }
+        });
+    });
+</script>
--- login.php
+++ login.php
@@ -1,24 +1,32 @@
 <?php
 include('connection.php');
 session_start();
-if(isset($_POST['StudentID'])){
-$StudentID = mysql_real_escape_string($_POST['StudentID']);
-$_Password = mysql_real_escape_string($_POST['password']);
+if (isset($_POST['StudentID'])) {
+    $StudentID = $_POST['StudentID'];
+    $_Password = $_POST['password'];
 
-		$LoginQuery = "SELECT COUNT(*) FROM studentinfotbl WHERE (StudentID = '$StudentID' AND Password = '$_Password')";
-		$LoginResult = mysql_query($LoginQuery);
-		$Row = mysql_fetch_array($LoginResult);
+    $student_tbl =
+        GSecureSQL::query(
+            "SELECT * FROM studentinfotbl WHERE StudentID = ?",
+            TRUE,
+            "s",
+            $StudentID
+        );
 
-		if($Row[0] > 0){
-				$_SESSION['StudentID'] = $StudentID;
-		        echo "
+    if (count($student_tbl)) {
+        if (hash('sha512', $_Password . $student_tbl[0]->SaltedPassword) == $student_tbl[0]->Password) {
+            $_SESSION['StudentID'] = $StudentID;
+            echo "
 		        <script type='text/javascript'>
 		        alert('You have successfully loggged in.');
 		        location.href='student-view/search-job/jobs.php';
 		        </script>";
-		        }
-		else{
-			echo "Incorrect Student ID or Password";
-			}
+        }
+        else{
+            echo "Incorrect Student ID or Password";
+        }
+    } else {
+        echo "Incorrect Student ID or Password";
+    }
 }
 ?>
