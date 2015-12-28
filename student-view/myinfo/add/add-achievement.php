4c495cbd245fb89bbd3dfce9ad8fc4ab76b7d58b
Aira Jane <aijeyn@gmail.com>
2015-12-29 01:27:15 +0800

    jobs.php done done done

--- student-view/myinfo/add/add-achievement.php
+++ student-view/myinfo/add/add-achievement.php
@@ -192,7 +192,7 @@ if(is_null($_SESSION['StudentID'])){
                                 <a href="../../applications/applications.php">Applications</a>
                             </li>
                             <li>
-                                <a href="../../search-job/search-job.php">Jobs</a>
+                                <a href="../../search-job/jobs.php">Jobs</a>
                             </li>
                             <li>
                                 <a href="../../settings/privacy-settings.php">Settings</a>
@@ -233,7 +233,7 @@ if(is_null($_SESSION['StudentID'])){
                         <a href="../../applications/applications.php">Applications</a>
                     </li>
                     <li>
-                        <a href="../../search-job/search-job.php">Jobs</a>
+                        <a href="../../search-job/jobs.php">Jobs</a>
                     </li>
                     <li>
                         <a href="../../settings/privacy-settings.php">Settings</a>
