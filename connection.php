82c772abb297cf6491d2014a38e2f3bcf5f44687
Mark Joseph Cinco <markjoseph1496@yahoo.com>
2015-12-28 23:08:21 +0800

    qwerty

--- connection.php
+++ connection.php
@@ -10,11 +10,9 @@ $user = "root";
 $password = "";
 $database = "e2edb";
 
-
 $bd = mysql_connect($hostname, $user, $password) or die ("Could not connect"); //connection of php to navicat
 //sensitive yung spacing... dito lang ^
 mysql_select_db($database, $bd) or die("could not select"); //selection ng DATABASE name w/c is mydatabase;
-
 
 
 class GSecureSQLConfig{
