5cd0a5b20a1e8fe3280a7dcf68cd062f087b8c12
ghabxph <ghabxph.official@gmail.com>
2016-01-04 16:20:18 +0800

    test

--- connection.php
+++ connection.php
@@ -1,33 +1,38 @@
 <?php
 
-class GSecureSQLConfig{
-    static function get_mysqli_config($type = NULL){
-        if(strtoupper($type) == 'LOCALHOST'){
+class GSecureSQLConfig
+{
+    static function get_mysqli_config($type = NULL)
+    {
+        if (strtoupper($type) == 'LOCALHOST') {
             return array(
-                'host'  => 'localhost:3306',
-                'user'  => 'root',
-                'pass'  => '',
-                'db'    => 'e2edb'
+                'host' => 'localhost:3306',
+                'user' => 'root',
+                'pass' => '',
+                'db' => 'e2edb'
             );
-        }else{
+        } else {
             return array(
-                'host'  => 'mysql.hostinger.ph',
-                'user'  => 'u987316885_e2edb',
-                'pass'  => 'o2ehQX3wqO',
-                'db'    => 'u987316885_e2edb'
+                'host' => 'mysql.hostinger.ph',
+                'user' => 'u987316885_e2edb',
+                'pass' => 'o2ehQX3wqO',
+                'db' => 'u987316885_e2edb'
             );
         }
     }
 }
 
-throw new Exception('test');
-class GSecureSQL{
+class GSecureSQL
+{
     private static $config;
-    private static function _get_mysqli_config(){
+
+    private static function _get_mysqli_config()
+    {
         self::$config = GSecureSQLConfig::get_mysqli_config();
     }
 
-    static function query($sql, $has_return = FALSE, $types = NULL){
+    static function query($sql, $has_return = FALSE, $types = NULL)
+    {
         // validation
 
         // -----
@@ -39,59 +44,55 @@ class GSecureSQL{
             self::$config['db']
         );
         $ret = NULL;
-        try{
-            $st = $cn->prepare($sql);
-            if(is_null($types)){
-                if(!$has_return){
-                    $st->execute();
-                    $st->close();
-                    $cn->close();
-                    $ret = 'Query has been executed';
-                }else{
-                    $st->execute();
-                    $result = $st->get_result();
-                    $st->close();
-                    $cn->close();
-                    $ret = array();
-                    while($field = $result->fetch_object()){
-                        array_push($ret, $field);
-                    }
-                }
-            }else{
-                if(!$has_return){
-                    $arg = func_get_args();
-                    $code = '$st->bind_param($types';
-                    for($i = 3; $i < count($arg); $i++){
-                        $code .= ',$arg[' . $i . ']';
-                    }
-                    $code .= ');';
-                    eval($code);
-                    $st->execute();
-                    $st->close();
-                    $cn->close();
-                }else{
-                    throw new Exception('Error in something');
-                    $arg = func_get_args();
-                    $code = '$st->bind_param($types';
-                    for($i = 3; $i < count($arg); $i++){
-                        $code .= ',$arg[' . $i . ']';
-                    }
-                    $code .= ');';
-                    eval($code);
-                    $st->execute();
-                    $result = $st->get_result();
-                    $st->close();
-                    $cn->close();
-                    $ret = array();
-                    while($field = $result->fetch_object()){
-                        array_push($ret, $field);
-                    }
+
+        $st = $cn->prepare($sql);
+        if (is_null($types)) {
+            if (!$has_return) {
+                $st->execute();
+                $st->close();
+                $cn->close();
+                $ret = 'Query has been executed';
+            } else {
+                $st->execute();
+                $result = $st->get_result();
+                $st->close();
+                $cn->close();
+                $ret = array();
+                while ($field = $result->fetch_object()) {
+                    array_push($ret, $field);
                 }
             }
-        }catch(Exception $e){
-            throw $e;
-        }finally{
-            return $ret;
+        } else {
+            if (!$has_return) {
+                $arg = func_get_args();
+                $code = '$st->bind_param($types';
+                for ($i = 3; $i < count($arg); $i++) {
+                    $code .= ',$arg[' . $i . ']';
+                }
+                $code .= ');';
+                eval($code);
+                $st->execute();
+                $st->close();
+                $cn->close();
+            } else {
+                throw new Exception('Error in something');
+                $arg = func_get_args();
+                $code = '$st->bind_param($types';
+                for ($i = 3; $i < count($arg); $i++) {
+                    $code .= ',$arg[' . $i . ']';
+                }
+                $code .= ');';
+                eval($code);
+                $st->execute();
+                $result = $st->get_result();
+                $st->close();
+                $cn->close();
+                $ret = array();
+                while ($field = $result->fetch_object()) {
+                    array_push($ret, $field);
+                }
+            }
         }
+        return $ret;
     }
 }
