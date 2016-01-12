a6311175c30ea7c716edc7f174cd293a46d36ddf
Aira Jane <aijeyn@gmail.com>
2016-01-12 21:57:53 +0800

    dcfdvdfvfdv

---
+++ student-view/notification/notification.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<?php
+include('../../connection.php');
+session_start();
+
+if (isset($_SESSION['StudentID'])) {
+    $StudentID = $_SESSION['StudentID'];
+} else {
+    $StudentID = '';
+    echo "
+        <script type='text/javascript'>
+        location.href='../../login-student.php?id=2';
+        </script>
+        ";
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+
+    <!-- Basic -->
+    <title>OJPMS</title>
+
+    <!-- Define Charset -->
+    <meta charset="utf-8">
+
+    <!-- Responsive Metatag -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Page Description and Author -->
+    <meta name="description" content="Margo - Responsive HTML5 Template">
+    <meta name="author" content="iThemesLab">
+
+    <!-- Bootstrap CSS -->
+    <link href="../../css/bootstrap.min.css" rel="stylesheet"/>
+
+    <!-- BootstrapValidator CSS -->
+    <link href="../../css/bootstrapValidator.min.css" rel="stylesheet"/>
+
+    <!-- jQuery and Bootstrap JS -->
+    <script type="text/javascript" src="../../js/jquery.min.js"></script>
+    <script type="text/javascript" src="../../js/bootstrap.min.js" ></script>
+
+    <!-- BootstrapValidator -->
+    <script src="../../js/bootstrapValidator.min.js" type="text/javascript"></script>
+
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="../../css/font-awesome.min.css" type="text/css" media="screen">
+
+    <!-- Fonts -->
+    <link href="../../fonts/ffonts/montserrat.css" rel="stylesheet" type="text/css">
+    <link href="../../fonts/ffonts/kaushan.css" rel="stylesheet" type="text/css">
+    <link href="../../fonts/ffonts/droid.css" rel="stylesheet" type="text/css">
+    <link href="../../fonts/ffonts/roboto.css" rel="stylesheet" type="text/css">
+
+    <!-- Slicknav -->
+    <link rel="stylesheet" type="text/css" href="../../css/slicknav.css" media="screen">
+
+    <!-- Margo CSS Styles  -->
+    <link rel="stylesheet" type="text/css" href="../../css/style.css" media="screen">
+
+    <!-- Responsive CSS Styles  -->
+    <link rel="stylesheet" type="text/css" href="../../css/responsive.css" media="screen">
+
+    <!-- Css3 Transitions Styles  -->
+    <link rel="stylesheet" type="text/css" href="../../css/animate.css" media="screen">
+
+    <!-- OJPMS CSS  -->
+    <link rel="stylesheet" type="text/css" href="../../css/ojpms-style.css" media="screen">
+
+    <!-- Color CSS Styles  -->
+    <link rel="stylesheet" type="text/css" href="../../css/colors/yellow.css" title="yellow" media="screen"/>
+
+    <!-- JS  -->
+    <script type="text/javascript" src="../../js/jquery.migrate.js"></script>
+    <script type="text/javascript" src="../../js/modernizrr.js"></script>
+    <script type="text/javascript" src="../../js/jquery.fitvids.js"></script>
+    <script type="text/javascript" src="../../js/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="../../js/nivo-lightbox.min.js"></script>
+    <script type="text/javascript" src="../../js/jquery.isotope.min.js"></script>
+    <script type="text/javascript" src="../../js/jquery.appear.js"></script>
+    <script type="text/javascript" src="../../js/count-to.js"></script>
+    <script type="text/javascript" src="../../js/jquery.textillate.js"></script>
+    <script type="text/javascript" src="../../js/jquery.lettering.js"></script>
+    <script type="text/javascript" src="../../js/jquery.easypiechart.min.js"></script>
+    <script type="text/javascript" src="../../js/jquery.nicescroll.min.js"></script>
+    <script type="text/javascript" src="../../js/jquery.parallax.js"></script>
+    <script type="text/javascript" src="../../js/jquery.slicknav.js"></script>
+</head>
+
+<body>
+    <div id="container">
+        <!-- Start Header Section -->
+        <div class="hidden-header"></div>
+        <header class="clearfix">
+        <!-- Start Top Bar -->
+            <div class="top-bar">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-7">
+                            <!-- Start Contact Info -->
+                            <ul class="contact-details">
+                                <li class="profile-name"><i class="fa fa-hashtag"></i> <b>008-2012-0805</b></li>
+                            </ul>
+                            <!-- End Contact Info -->
+                        </div>
+                        <!-- .col-md-6 -->
+                        <div class="col-md-5">
+                        <!-- Start Social Links -->
+                            <ul class="social-list">
+                                <li class="profile-name">
+                                    <a class="bell itl-tooltip" data-placement="bottom" title="" href="#" data-original-title="Notification"><i class="fa fa-bell"></i></a>
+                                </li>
+                                <li class="profile-name">
+                                    &nbsp;
+                                </li>
+                                <li class="profile-name">
+                                    <i class="fa fa-user"></i> Hello, <b>Aira Jane Cruz</b>                               
+                                </li>
+                            </ul>
+                            <!-- End Social Links -->
+                        </div>
+                        <!-- .col-md-6 -->
+                    </div>
+                    <!-- .row -->
+                </div>
+                <!-- .container -->
+            </div>
+            <!-- .top-bar -->
+            <!-- End Top Bar -->
+
+            <!-- Start  Logo & Naviagtion  -->
+            <div class="navbar navbar-default navbar-top">
+                <div class="container">
+                    <div class="navbar-header">
+                    <!-- Stat Toggle Nav Link For Mobiles -->
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <i class="fa fa-bars"></i>
+                        </button>
+                        <!-- End Toggle Nav Link For Mobiles -->
+                        <a class="navbar-brand" href="">
+                            <img src="../../images/ojpms.png">
+                        </a>
+                    </div>
+                    <div class="navbar-collapse collapse">
+                        <!-- Sign-out -->
+                        <div class="signout-side">
+                            <a href="#" class="btn-system btn-mini border-btn">Apply Now</a>
+                        </div>
+                        <!-- End Sign-out -->
+                    </div>
+                </div>
+                <!-- Mobile Menu Start -->
+                <ul class="wpb-mobile-menu">
+                    <li>
+                        <a href="#">Apply Now</a>
+                    </li>
+                    <li>
+                        <a href="../login-student.php?id=1"><i class="fa fa-sign-out"></i> Sign Out</a>
+                    </li>
+                </ul>
+                <!-- Mobile Menu End -->
+            </div>
+        </header>
+
+        <div class="middle-bar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-5">
+                        <ul class="mid-list">
+                            &nbsp;
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+
+        <!-- Start Content -->
+        <div id="content">
+            <div class="container">
+                <div class="row sidebar-page">
+                    <!-- Page Content -->
+                    <div class="col-md-9 page-content">
+                        
+                    </div>
+                    <!-- End Page Content -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Content -->
+    <script type="text/javascript" src="../../js/script.js"></script>
+
+
+    <!-- Start Footer Section -->
+    <footer>
+        <div class="container">
+            <!-- Start Copyright -->
+            <div class="copyright-section">
+                <div class="row">
+                    <div class="col-md-6">
+                        <p>&copy; 2015 OJPMS - All Rights Reserved</p>
+                    </div>
+                    <!-- .col-md-6 -->
+                    <div class="col-md-6">
+                        <ul class="footer-nav">
+                            <li><a href="#">Sitemap</a>
+                            </li>
+                            <li><a href="#">Privacy Policy</a>
+                            </li>
+                            <li><a href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- .col-md-6 -->
+                </div>
+                <!-- .row -->
+            </div>
+            <!-- End Copyright -->
+        </div>
+    </footer>
+    <!-- End Footer Section -->
+
+    <!-- Go To Top Link -->
+    <a href="#" class="back-to-top"><i class="fa fa-angle-up"></i></a>
+
+    <div id="loader">
+        <div class="spinner">
+            <div class="dot1"></div>
+            <div class="dot2"></div>
+        </div>
+    </div>
+</body>
+</html>
