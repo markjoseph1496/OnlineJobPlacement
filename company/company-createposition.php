a6d21dac28323407304d0af352d62a879f17e64a
Mark Joseph Cinco <markjoseph1496@yahoo.com>
2016-01-14 19:21:43 +0800

    Polishing

--- company/company-createposition.php
+++ company/company-createposition.php
@@ -4,10 +4,6 @@ session_start();
 $CompanyID = $_SESSION['CompanyID'];
 ?>
 <!doctype html>
-<!--[if IE 8 ]>
-<html class="ie ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]>
-<html lang="en" class="no-js"> <![endif]-->
 <html lang="en">
 
 <head>
--- registeradd.php
+++ registeradd.php
@@ -1,55 +1,54 @@
 <?php
 include('connection.php');
 
-if(isset($_POST['type'])){
-	$isAvailable = true;
+if (isset($_POST['type'])) {
+    $isAvailable = true;
     $StudentID = $_POST['StudentID'];
     $query = "SELECT StudentID FROM studentinfotbl WHERE StudentID = '$StudentID'";
     $Result = mysql_query($query);
-    if($Result){
-        if(mysql_num_rows($Result) == 0){
-        	$isAvailable = true;
-        }
-        else{
-        	$isAvailable = false;
+    if ($Result) {
+        if (mysql_num_rows($Result) == 0) {
+            $isAvailable = true;
+        } else {
+            $isAvailable = false;
         }
     }
-	echo json_encode(array(
-	    'valid' => $isAvailable,
-	));
+    echo json_encode(array(
+        'valid' => $isAvailable,
+    ));
 }
 
-if(isset($_POST['btnRegister'])){
-	$StudentID = $_POST['StudentID'];
-	$FirstName = $_POST['FirstName'];
-	$LastName = $_POST['LastName'];
-	$Birthday = $_POST['Birthday'];
-	$MobileNumber = $_POST['MobileNumber'];
-	$Email = $_POST['Email'];
-	$Password = $_POST['_Password'];
-	$City = $_POST['City'];
-	$EducAttain = $_POST['EducAttain'];
-	$Course = $_POST['Course'];
-	$GraduatedMonth = $_POST['GraduatedMonth'];
-	$GraduatedYear = $_POST['GraduatedYear'];
+if (isset($_POST['resumelink'])) {
+    $StudentID = $_POST['StudentID'];
+    $FirstName = $_POST['FirstName'];
+    $LastName = $_POST['LastName'];
+    $Birthday = $_POST['Birthday'];
+    $MobileNumber = $_POST['MobileNumber'];
+    $Email = $_POST['Email'];
+    $Password = $_POST['_Password'];
+    $City = $_POST['City'];
+    $EducAttain = $_POST['EducAttain'];
+    $Course = $_POST['Course'];
+    $GraduatedMonth = $_POST['GraduatedMonth'];
+    $GraduatedYear = $_POST['GraduatedYear'];
 
     $salt = hash('sha512', mt_rand(0, PHP_INT_MAX) . mt_rand(0, PHP_INT_MAX) . mt_rand(0, PHP_INT_MAX));
     $Password = hash('sha512', $Password . $salt);
 
-    $yeargraduated = $GraduatedMonth ." ". $GraduatedYear;
+    $yeargraduated = $GraduatedMonth . " " . $GraduatedYear;
     $EducAttain = mysql_real_escape_string($EducAttain);
 
     $query = "INSERT INTO studentinfotbl (StudentID,FirstName,LastName,Birthdate,Password,SaltedPassword,EmploymentStatus,MajorCourse) values  ('$StudentID','$FirstName','$LastName','$Birthday','$Password','$salt','Unemployed','$Course')";
     $query1 = "INSERT INTO schooltbl (StudentID,School,Attainment,Course,Graduated) values  ('$StudentID','STI College Caloocan','$EducAttain','$Course','$yeargraduated')";
     $query2 = "INSERT INTO studcontactstbl (StudentID,Email,MobileNumber,City) values  ('$StudentID','$Email','$MobileNumber','$City')";
 
-	$Result = mysql_query($query);
-	$Result1 = mysql_query($query1);
-	$Result2 = mysql_query($query2);
-        echo "
+    $Result = mysql_query($query);
+    $Result1 = mysql_query($query1);
+    $Result2 = mysql_query($query2);
+    echo "
             <script type='text/javascript'>
             alert('You have successfully Registered.');
-            location.href='./login-student.php';
+            location.href='./registration.php';
             </script>
             ";
 }
