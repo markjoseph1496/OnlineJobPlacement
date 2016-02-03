a5bbae6506317c162bad25cb0c2a8faf3bdcaae8
Mark Joseph Cinco <markjoseph1496@yahoo.com>
2016-02-03 12:55:46 +0800

    Polishing

---
+++ student-view/myinfo/add/upload.php
@@ -0,0 +1,38 @@
+<?php
+include('../../../connection.php');
+session_start();
+$StudentID = $_SESSION['StudentID'];
+
+$fileToUpload = basename($_FILES["DocumentFile"]["name"]);
+$target_dir = "Documents/";
+$target_file = $target_dir . $fileToUpload;
+$uploadOk = 1;
+
+
+$salt = hash('sha512', mt_rand(0, PHP_INT_MAX));
+$target_file = hash('sha512', $target_file . $salt);
+
+echo $fileToUpload;
+echo $target_file;
+die();
+
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+
+} else {
+    if (move_uploaded_file($_FILES["DocumentFile"]["tmp_name"], $target_file)) {
+
+        GSecureSQL::query(
+            "INSERT INTO documentstbl (StudentID, DocumentName, DocumentPath) VALUES (?,?,?)",
+            FALSE,
+            "sss",
+            $StudentID,
+            $fileToUpload,
+            $target_file
+        );
+        header("location: portfolio.php");
+
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
