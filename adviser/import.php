424a5bc5d03fce5434e20c1502c8f7ac653e5670
princetj <princetj.21@gmail.com>
2016-02-04 12:11:35 +0800

    tj

--- adviser/import.php
+++ adviser/import.php
@@ -1,47 +1,53 @@
 <?php
-include '../connection.php';
-if(isset($_POST["Import"])){
-		
-
-		echo $filename=$_FILES["file"]["tmp_name"];
-		
-
-		 if($_FILES["file"]["size"] > 0)
-		 {
-
-		  	$file = fopen($filename, "r");
-	         while (($emapData = fgetcsv($file, 10000, ",")) !== FALSE)
-	         {
-	         		    
-	          //It wiil insert a row to our subject table from our csv file`
-	           $sql = "INSERT into ojttbl (`StudentID`,`LastName`,FirstName`,`MiddleName`,`Course`,`CompanyName`,`CompanyAddress`,`Supervisor`,`Position`,`ContactNumber`,`Email`) 
-	            	values('$emapData[0]','$emapData[1]','$emapData[2]','$emapData[3]','$emapData[4]','$emapData[5]','$emapData[6]','$emapData[7]','$emapData[8]','$emapData[9]','$emapData[10]')";
-	         //we are using mysql_query function. it returns a resource on true else False on error
-	          $result = mysql_query( $sql, $bd );
-				if(! $result )
-				{
-					echo "<script type=\"text/javascript\">
-							alert(\"Invalid File:Please Upload CSV File.\");
-							window.location = \"ojt-adviser.php\"
-						</script>";
-				
-				}
-
-	         }
-	         fclose($file);
-	         //throws a message if data successfully imported to mysql database from excel file
-	         echo "<script type=\"text/javascript\">
-						alert(\"CSV File has been successfully Imported.\");
-						window.location = \"ojt-adviser.php\"
-					</script>";
-	        
-			 
-
-			 //close of bdection
-			mysql_close($bd); 
-				
-		 	
+include ("../connection.php");
+	
+	if(isset($_POST["Import"]))
+	{
+		$file = $_FILES['file']['tmp_name'];
+		$handle = fopen($file, "r");
+		$c = 0;
+		while(($filesop = fgetcsv($handle, 1000, ",")) !== false)
+		{
+			$StudentID = $filesop[0];
+	         	$LastName = $filesop[1];
+	         	$FirstName = $filesop[2];
+	         	$MiddleName = $filesop[3];
+	         	$Course = $filesop[4];
+	         	$CompanyName = $filesop[5];
+	         	$CompanyAddress = $filesop[6];
+	         	$Supervisor = $filesop[7];
+	         	$Position = $filesop[8];
+	         	$ContactNumber = $filesop[9];
 			
-		 }
-	}	 
+			$sql = mysql_query("INSERT INTO ojttbl (
+				StudentID, 
+				LastName, 
+				FirstName, 
+				MiddleName,
+				Course, 
+				CompanyName, 
+				CompanyAddress, 
+				Position, 
+				ContactNumber) 
+				VALUES (
+					'$StudentID',
+					'$LastName',
+					'$FirstName',
+					'$MiddleName',
+					'$Course',
+					'$CompanyName',
+					'$CompanyAddress',
+					'$Supervisor',
+					'$Position',
+					'$ContactNumber')");
+			$c = $c + 1;
+		}
+		
+			if($sql){
+				echo "You database has imported successfully. You have inserted ". $c ." recoreds";
+			}else{
+				echo "Sorry! There is some problem.";
+			}
+
+	}
 ?>		 
