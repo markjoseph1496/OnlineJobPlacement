f381472a501014d4bed22763a16b5a4f2af3cc1b
Mark Joseph Cinco <markjoseph1496@yahoo.com>
2016-02-08 07:23:57 +0800

    Polishing

--- student-view/resume/resume.php
+++ student-view/resume/resume.php
@@ -45,7 +45,7 @@ $progress_tbl =
         Specialization,
         Languages,
         _References
-        FROM Progresstbl
+        FROM progresstbl
         WHERE StudentID = ?",
         TRUE,
         "s",
