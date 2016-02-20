54b4f1f6ff200bc5f57a3b09070998d002af02e4
Aira Jane <aijeyn@gmail.com>
2016-02-20 22:12:52 +0800

    loginstudent

--- login-student.php
+++ login-student.php
@@ -28,6 +28,9 @@
 </head>
 <body class="login-student">
     <div class="box effect3" style="margin-top:5%;">
+        <a href="index.php"><img src="images/logo/ojpms-cube.png" title="OJPMS - Home"style="width:20%;height:20%;"></a>
+    </div>
+    <div class="box effect3" style="margin-top:1%;">
         <h2 class="register-title">Sign In</h2>
         <form id="myForm" action="login.php" method="POST" autocomplete="off">
             <div id="message" style="color:#d95c5c;font-weight:600;"></div>
@@ -43,7 +46,7 @@
         </div>
         
         <button type="submit" class="btn btn--green register-submit text-center" id="button"><center>Sign In</center></button>
-        <p class="register-terms">Don't have an account? Get started <a href="registration.php">here</a>.</p>
+        <p class="register-terms">Don't have an account? Get started <a href="registration.php"><u>here</u></a>.</p>
     </div>
 </body>
 </html>
