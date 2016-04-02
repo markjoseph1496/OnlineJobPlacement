017d703795248143876e54e71803d4e03f1e6369
Mark Joseph Cinco <markjoseph1496@yahoo.com>
2016-04-02 21:15:43 +0800

    Polishing

--- test.php
+++ test.php
@@ -3,11 +3,11 @@
 include('xmlapi-php-master/xmlapi.php');
 
 $cpanelusr = 'judeecaballero@gmail.com';
-$cpanelpass = 'judeelovejesus';
+$cpanelpass = 'judeelovedjesus';
 $xmlapi = new xmlapi('mx1.hostinger.ph');
 $xmlapi->set_port( 2525 );
 $xmlapi->password_auth($cpanelusr,$cpanelpass);
 $xmlapi->set_debug(0); //output actions in the error log 1 for true and 0 false
-$result = $xmlapi->api1_query($cpanelusr, 'SubDomain', 'addsubdomain', array('subdomainname','domain.com',0,0, '/public_html/subdomains/subdomainname'));
+$result = $xmlapi->api1_query($cpanelusr, 'SubDomain', 'addsubdomain', array('subdomainname','teachermanagementsystem.com',0,0, '/public_html/subdomains/subdomainname'));
 ?>
     
