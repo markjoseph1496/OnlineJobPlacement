da99004e270577adb44271ee082ac2206e3b4c62
Mark Joseph Cinco <markjoseph1496@yahoo.com>
2016-04-02 21:06:31 +0800

    Polishing

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ include('xmlapi-php-master/xmlapi.php');
 
 $cpanelusr = 'judeecaballero@gmail.com';
 $cpanelpass = 'judeelovejesus';
-$xmlapi = new xmlapi('127.0.0.1');
+$xmlapi = new xmlapi('31.220.110.19');
 $xmlapi->set_port( 2083 );
 $xmlapi->password_auth($cpanelusr,$cpanelpass);
 $xmlapi->set_debug(0); //output actions in the error log 1 for true and 0 false
