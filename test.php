ad062ebbd0e7246257dbfeadd5f5968d01b3f94e
Mark Joseph Cinco <markjoseph1496@yahoo.com>
2016-04-02 21:02:10 +0800

    Polishing

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+
+include('xmlapi-php-master/xmlapi.php');
+
+$cpanelusr = 'judeecaballero@gmail.com';
+$cpanelpass = 'judeelovejesus';
+$xmlapi = new xmlapi('127.0.0.1');
+$xmlapi->set_port( 2083 );
+$xmlapi->password_auth($cpanelusr,$cpanelpass);
+$xmlapi->set_debug(0); //output actions in the error log 1 for true and 0 false
+$result = $xmlapi->api1_query($cpanelusr, 'SubDomain', 'addsubdomain', array('subdomainname','domain.com',0,0, '/public_html/subdomains/subdomainname'));
+?>
+    
