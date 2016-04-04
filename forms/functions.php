1b767b1cdaef60b2b7c2d99ac524dccf32dd4df7
GhabXPH <ghabxph.official@gmail.com>
2016-04-04 16:49:19 +0800

    Polishing

--- forms/functions.php
+++ forms/functions.php
@@ -1,5 +1,6 @@
 <?php
 include('../connection.php');
+include('../common-functions.php');
 
 if(isset($_POST['FormSubmit'])){
     $CompanyName = ucwords($_POST['CompanyName']);
@@ -25,6 +26,64 @@ if(isset($_POST['FormSubmit'])){
     $OthersExtra = "";
     $RoomForExam = "";
     $DoorPrizes = "";
+
+    $a = strlen($CompanyName) > 0;
+    $a = $a && (strlen($CompanyAddress) > 0);
+    $a = $a && (strlen($City) > 0);
+    $a = $a && (strlen($Website) > 0);
+    $a = $a && (strlen($ContactPerson) > 0);
+    $a = $a && (strlen($Designation) > 0);
+    $a = $a && (strlen($Email) > 0);
+    $a = $a && (strlen($Phone1) > 0);
+    $a = $a && (strlen($Industry) > 0);
+    $a = $a && (strlen($Representative1) > 0);
+
+    if($a === FALSE){
+        header('Location: ../jobfair?error');
+        die();
+    }
+
+    $validation_config = array(
+        'ContactPerson' => array(
+            'pattern' => '/^[a-z\s]+$/i',
+            'errorMsg' => ''
+        ),
+        'Phone1' => array(
+            'pattern' => '/(^(0(9(05|06|07|08|09|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|32|33|34|35|36|37|38|39|42|43|46|47|48|49|75|77|89|94|96|97|98)[0-9]{7}|[0-8][0-9]{5})|[1-9][0-9]{6})$|^$)/',
+            'errorMsg' => ''
+        ),
+        'Phone2' => array(
+            'pattern' => '/(^(0(9(05|06|07|08|09|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|32|33|34|35|36|37|38|39|42|43|46|47|48|49|75|77|89|94|96|97|98)[0-9]{7}|[0-8][0-9]{5})|[1-9][0-9]{6})$|^$)/',
+            'errorMsg' => ''
+        ),
+        'Phone3' => array(
+            'pattern' => '/(^(0(9(05|06|07|08|09|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|32|33|34|35|36|37|38|39|42|43|46|47|48|49|75|77|89|94|96|97|98)[0-9]{7}|[0-8][0-9]{5})|[1-9][0-9]{6})$|^$)/',
+            'errorMsg' => ''
+        ),
+        'MobileNumber' => array(
+            'pattern' => '/(^(0(9(05|06|07|08|09|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|32|33|34|35|36|37|38|39|42|43|46|47|48|49|75|77|89|94|96|97|98)[0-9]{7}|[0-8][0-9]{5})|[1-9][0-9]{6})$|^$)/',
+            'errorMsg' => ''
+        ),
+        'FaxNumber' => array(
+            'pattern' => '/^([0-9]+[.]*)+$/i',
+            'errorMsg' => ''
+        ),
+        'Representative1' => array(
+            'pattern' => '/^[a-z\s]+$/i',
+            'errorMsg' => ''
+        ),
+        'Representative2' => array(
+            'pattern' => '/^[a-z\s]+$/i',
+            'errorMsg' => ''
+        )
+    );
+
+
+    $validation_return = $common_functions->validate($_POST, $validation_config);
+    if($validation_return['hasError']){
+        header("location: ../jobfair?error");
+        die();
+    }
 
     if(isset($_POST['OthersCB'])){
         $Others = ucwords($_POST['Others']);
